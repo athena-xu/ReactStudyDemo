870fa24c2298857d5768c0146cdb25b56eba9933
许姝君 <xusj34037@hundsun.com>
2022-05-23 17:49:31 +0800

    2022/05/23

--- src/pages/cssAndStudy/modal.js
+++ src/pages/cssAndStudy/modal.js
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 
+const ModalWrapper = styled.div`
+  position: absolute;
+  left: calc(50% - 200px);
+  top: calc(50% - 150px);
+  width: 400px;
+  height: 300px;
+  background-color: rgba(0,0,0,0.7);
+`
+
+// all in js 理念；styled-components 库
 export default class Modal extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
-      <div>
+      <ModalWrapper>
         Modal弹窗
-      </div>
+      </ModalWrapper>
     );
   }
 }
